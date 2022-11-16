f6acb084e98adaeb2a44a459a5150badc5b37e68
rat <112645929+iLoveRat@users.noreply.github.com>
2022-11-16 08:23:07 +0000

    Update FunnyDeath.js

--- Mushroom/Code/Other/FunnyDeath.js
+++ Mushroom/Code/Other/FunnyDeath.js
@@ -1,24 +1,24 @@
 import Config from "../../Config/Config"
+import {mc} from "../Utils"
+
+const ChatComponentText = Java.type("net.minecraft.util.ChatComponentText");
 
 register("chat", (ripbozo) => {
-    if (!Config.omegahilarity) return;
-    let fullMessage = ChatLib.getChatMessage(ripbozo).toLowerCase();
-    if (fullMessage.indexOf("You") && fullMessage.indexOf("killed by")) {
-        if (fullMessage.indexOf("and became a ghost.") !== 0) {
-        banscreen();
-        }
-        else {
-            if (!Config.largehilarity) return;
-        }
-    }
-}).setCriteria("boss message")
+  if (!Config.omegahilarity) return;
+    banscreenpog();
+}).setCriteria("You were killed ${ripbozo}")
 
-function banscreen(){
-    banned = new ChatComponentText("§cYou are temporarily banned for §f29d 23h 59m 59s §cfrom this server!\n\n§7Reason: §rCheating through the use of unfair game advantages.\n§7Find out more: §b§nhttps://www.hypixel.net/appeal\n\n§7Ban ID: §r#" + makeid()+"\n§7Sharing your Ban ID may affect the processing of your appeal!")
+register("chat", (killedby) => {
+  if (!Config.largehilarity) return;
+    banscreenpog();
+}).setCriteria(" ☠ You were ${killedby} and became a ghost.")
+
+function banscreenpog(){
+    banned = new ChatComponentText("§cYou are temporarily banned for §f29d 23h 59m 59s §cfrom this server!\n\n§7Reason: §rCheating through the use of unfair game advantages.\n§7Find out more: §b§nhttps://www.hypixel.net/appeal\n\n§7Ban ID: §r#" + makeid2()+"\n§7Sharing your Ban ID may affect the processing of your appeal!")
     mc.func_147114_u().func_147298_b().func_150718_a(banned)
   }
 
-function makeid() {
+function makeid2() {
   var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789012345678901234567890123456789';
   var charactersLength = characters.length;
