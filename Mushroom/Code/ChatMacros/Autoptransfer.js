ba65d74602a47d92363e06f2f89d2b320af0f230
scale <113771222+scoliosiss@users.noreply.github.com>
2022-12-26 18:15:15 +0200

    Adding credits

--- Mushroom/Code/ChatMacros/Autoptransfer.js
+++ Mushroom/Code/ChatMacros/Autoptransfer.js
@@ -1,6 +1,9 @@
 import Config from "../../Config/Config"
 import { stripRank } from "../Utils"
 
+// CODE STOLEN FROM UNCLAIMEDBLOOM
+// please check out his modules on the chattriggers website!
+
 register("chat", (p1, p2) => {
     if (stripRank(p1) == Player.getName() && Config.autoTransfer) ChatLib.command(`p transfer ${stripRank(p2)}`)
 }).setChatCriteria("The party was transferred to ${p1} by ${p2}")
